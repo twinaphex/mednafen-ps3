61b89979c19d121b265d057ba1b3c2e51d581cfc
mike_kobb <mike_kobb@yahoo.com>
2010-12-29 21:26:50 -0500

    Remove unsigned Clamp

--- system/sdl/utility.h
+++ system/sdl/utility.h
@@ -104,14 +104,7 @@ class				Utility
 			return aValue;
 		}
 		
-		static uint32_t					Clamp						(uint32_t aValue, uint32_t aMin, uint32_t aMax)
-		{
-			if(aValue < aMin)		return aMin;
-			if(aValue > aMax)		return aMax;
-			return aValue;
-		}
 };
-
 class				PathBuild
 {
 	public:
