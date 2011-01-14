c08a75562c8b14c43918ff485033ef9f8f3fd06f
Mike <Mike@localhost>
2011-01-14 04:03:42 -0500

    Remove unneeded files

--- mednafen/src/build/cputest.d
+++
@@ -1,4 +0,0 @@
-cputest.o: /Users/jason/Projects/mednafen-ps3/mednafen/src/cputest.cpp \
- /Users/jason/Projects/mednafen-ps3/mednafen/src/cputest.h
-
-/Users/jason/Projects/mednafen-ps3/mednafen/src/cputest.h:
