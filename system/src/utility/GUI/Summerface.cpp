178aa06db210c3ca09883391a14b3e1910114895
Mike <Mike@localhost>
2011-01-15 10:19:16 -0500

    Fill out Summerface some

--- system/src/utility/GUI/Summerface.cpp
+++ system/src/utility/GUI/Summerface.cpp
@@ -1,17 +1,49 @@
 #include <ps3_system.h>
 
-											SummerfaceWindow::SummerfaceWindow					(Summerface* aInterface, const Area& aRegion)
+uint32_t ptpp(uint32_t aIn, bool aX)
 {
-	Interface = aInterface;
-	Region = aRegion;
+	uint32_t s = aX ? es_video->GetScreenWidth() : es_video->GetScreenHeight();
+	return s * ((double)aIn / 100.0);
+}
 
-	Client = Area(Region.X + BorderWidth, Region.Y + BorderWidth, Region.Width - BorderWidth * 2, Region.Height - BorderWidth * 2);
+											SummerfaceWindow::SummerfaceWindow					(const Area& aRegion)
+{
+	Interface = 0;
+
+	Area outregion(ptpp(aRegion.X, 1), ptpp(aRegion.Y, 0), ptpp(aRegion.Width, 1), ptpp(aRegion.Height, 0));
+
+	Region = outregion;
+	Client = Area(outregion.X + BorderWidth, outregion.Y + BorderWidth, outregion.Width - BorderWidth * 2, outregion.Height - BorderWidth * 2);
 }
 
 											SummerfaceWindow::~SummerfaceWindow					()
 {
 
 }
+
+Summerface*									SummerfaceWindow::GetInterface						()
+{
+	return Interface;
+}
+
+std::string									SummerfaceWindow::GetName							()
+{
+	return Name;
+}
+
+void										SummerfaceWindow::SetInterface						(Summerface* aInterface, const std::string& aName)
+{
+	if(!Interface)
+	{
+		Interface = aInterface;
+		Name = aName;
+	}
+	else
+	{
+		throw ESException("SummerfaceWindow::SetInterface may only be called once per window. Once it's assigned it belongs to its parent forever.");
+	}
+}
+
 
 //Draw the border and background, leave clip set to the windows client area
 bool										SummerfaceWindow::PrepareDraw						()
@@ -41,7 +73,105 @@ bool										SummerfaceWindow::PrepareDraw						()
 
 }
 
-											SummerfaceGrid::SummerfaceGrid						(Summerface* aInterface, const Area& aRegion, uint32_t aWidth, uint32_t aHeight) : SummerfaceWindow(aInterface, aRegion)
+											SummerfaceStaticConduit::SummerfaceStaticConduit	(bool (*aCallback)(void*, const std::string&, SummerfaceItem*), void* aUserData)
+{
+	Callback = aCallback;
+	UserData = aUserData;
+}
+
+bool										SummerfaceStaticConduit::HandleInput				(const std::string& aWindow, SummerfaceItem* aItem)
+{
+	if(Callback)
+	{
+		Callback(UserData, aWindow, aItem);
+	}
+}
+
+											SummerfaceList::SummerfaceList						(const Area& aRegion) : SummerfaceWindow(aRegion)
+{
+	InputHandler = 0;
+	SelectedIndex = 0;
+	DeleteInputHandler = false;
+
+	Canceled = false;
+
+	LabelFont = FontManager::GetFixedFont();
+}
+
+											SummerfaceList::~SummerfaceList						()
+{
+	for(std::vector<SummerfaceItem*>::iterator iter = Items.begin(); iter != Items.end(); iter ++)
+	{
+		delete (*iter);
+	}
+
+	if(InputHandler && DeleteInputHandler)
+	{
+		delete InputHandler;
+	}
+}
+
+void										SummerfaceList::AddItem								(SummerfaceItem* aItem)
+{
+	Items.push_back(aItem);
+}
+
+SummerfaceItem*								SummerfaceList::GetSelected							()
+{
+	if(SelectedIndex < Items.size())
+	{
+		return Items[SelectedIndex];
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+uint32_t									SummerfaceList::GetItemCount						()
+{
+	return Items.size();
+}
+
+void										SummerfaceList::SetSelection						(uint32_t aIndex)
+{
+	if(aIndex >= Items.size())
+	{
+		throw ESException("WinterfaceList: Item index out of range [Item %d, Total %d]", aIndex, Items.size());
+	}
+
+	SelectedIndex = aIndex;
+}
+
+
+bool										SummerfaceList::WasCanceled							()
+{
+	return Canceled;
+}
+
+void										SummerfaceList::SetInputConduit						(SummerfaceInputConduit* aInputConduit, bool aDelete)
+{
+	if(InputHandler && DeleteInputHandler)
+	{
+		delete InputHandler;
+	}
+
+	InputHandler = aInputConduit;
+	DeleteInputHandler = aDelete;
+}
+
+SummerfaceInputConduit*						SummerfaceList::GetInputConduit						()
+{
+	return InputHandler;
+}
+
+void										SummerfaceList::SetFont								(Font* aFont)
+{
+	LabelFont = aFont;
+}
+
+
+											SummerfaceGrid::SummerfaceGrid						(const Area& aRegion, uint32_t aWidth, uint32_t aHeight, bool aHeader, bool aLabels) : SummerfaceList(aRegion)
 {
 	if(aWidth == 0 || aHeight == 0 || aWidth > 16 || aHeight > 16)
 	{
@@ -51,22 +181,18 @@ bool										SummerfaceWindow::PrepareDraw						()
 	Width = aWidth;
 	Height = aHeight;
 	
-	XSelection = 0;
-	YSelection = 0;
-	SelectedIndex = 0;
-	Canceled = false;
+	SetDrawMode(aHeader, aLabels);
 }
 
 											SummerfaceGrid::~SummerfaceGrid						()
 {
-	for(std::vector<SummerfaceItem*>::iterator iter = Items.begin(); iter != Items.end(); iter ++)
-	{
-		delete (*iter);
-	}
 }
 
 bool										SummerfaceGrid::Input								()
 {
+	uint32_t XSelection = SelectedIndex % Width;
+	uint32_t YSelection = SelectedIndex / Width;
+
 	XSelection += es_input->ButtonDown(0, ES_BUTTON_RIGHT) ? 1 : 0;
 	XSelection -= es_input->ButtonDown(0, ES_BUTTON_LEFT) ? 1 : 0;
 	XSelection = Utility::Clamp(XSelection, 0, (int32_t)Width - 1);
@@ -83,6 +209,11 @@ bool										SummerfaceGrid::Input								()
 		return true;
 	}
 
+	if(InputHandler && SelectedIndex < Items.size())
+	{
+		return InputHandler->HandleInput(GetName(), Items[SelectedIndex]); 
+	}
+
 	return false;
 }
 
@@ -92,22 +223,32 @@ bool										SummerfaceGrid::DrawItem							(SummerfaceItem* aItem, uint32_t aX
 	uint32_t TextColor = 0xFFFFFFFF;
 	Area ImageArea(0, 0, 0, 0);
 
-	if(image && aWidth != 0 && aHeight)
+	if(image && aWidth && aHeight)
 	{
 		if(ImageArea.Width == 0)
 		{
 			ImageArea = Area(0, 0, image->GetWidth(), image->GetHeight());
 		}
 
+		if(DrawLabels)
+		{
+			aHeight -= LabelFont->GetHeight();
+		}
+
 		uint32_t x = aX, y = aY, w = aWidth, h = aHeight;
 		Utility::CenterAndScale(x, y, w, h, ImageArea.Width, ImageArea.Height);
 		
 		es_video->PlaceTexture(image, x, y, w, h, 0xFFFFFFFF, &ImageArea);
+
+		if(DrawLabels)
+		{
+			LabelFont->PutString(aItem->GetText().c_str(), aX, aY + aHeight, 0xFFFFFFFF);
+		}
 	}
 	
 	if(aSelected)
 	{
-		es_video->FillRectangle(Area(aX, aY, aWidth, aHeight), Colors::SpecialBackGround);
+		es_video->FillRectangle(Area(aX, aY, aWidth - 2, aHeight - 2), Colors::SpecialBackGround);
 	}
 
 	return false;
@@ -118,6 +259,18 @@ bool										SummerfaceGrid::Draw								()
 	uint32_t iconWidth = es_video->GetClip().Width / Width - 4;
 	uint32_t iconHeight = es_video->GetClip().Height / Height - 4;	
 
+	uint32_t XSelection = SelectedIndex % Width;
+	uint32_t YSelection = SelectedIndex / Width;
+
+	if(DrawHeader)
+	{
+		iconHeight = (es_video->GetClip().Height - LabelFont->GetHeight()) / Height - 4;
+		if(SelectedIndex < Items.size())
+		{
+			LabelFont->PutString(Items[SelectedIndex]->GetText().c_str(), 0, 0, 0xFFFFFFFF);
+		}
+	}
+
 	for(int i = 0; i != Height; i ++)
 	{
 		for(int j = 0; j != Width; j ++)
@@ -127,33 +280,115 @@ bool										SummerfaceGrid::Draw								()
 				break;
 			}
 		
-			DrawItem(Items[i * Width + j], j * iconWidth + 4, i * iconHeight + 4, iconWidth, iconHeight, j == XSelection && i == YSelection);
+			DrawItem(Items[i * Width + j], j * iconWidth + 4, i * iconHeight + 4 + (DrawHeader ? LabelFont->GetHeight() : 0), iconWidth, iconHeight, j == XSelection && i == YSelection);
 		}
 	}
 	
 	return false;
 }
 
-uint32_t									SummerfaceGrid::GetItemCount						()
+void										SummerfaceGrid::SetDrawMode							(bool aHeader, bool aLabels)
 {
-	return Items.size();
+	DrawHeader = aHeader;
+	DrawLabels = aLabels;
 }
 
-void										SummerfaceGrid::SetSelection						(uint32_t aIndex)
+											SummerfaceLineList::SummerfaceLineList				(const Area& aRegion) : SummerfaceList(aRegion)
 {
-	if(aIndex >= Items.size())
+
+}
+
+											SummerfaceLineList::~SummerfaceLineList				()
+{
+}
+
+bool										SummerfaceLineList::DrawItem						(SummerfaceItem* aItem, uint32_t aX, uint32_t aY, bool aSelected)
+{
+	Texture* image = ImageManager::GetImage(aItem->GetImage());
+
+	if(image)
 	{
-		throw ESException("WinterfaceIconGrid: Item index out of range [Item %d, Total %d]", aIndex, Items.size());
+		uint32_t width = (uint32_t)((double)image->GetWidth() * ((double)(LabelFont->GetHeight() - 4) / (double)image->GetHeight()));
+
+		es_video->PlaceTexture(image, aX, aY + 2, width, LabelFont->GetHeight() - 4);
+		aX += width;
 	}
 
-	SelectedIndex = aIndex;
-	XSelection = aIndex % Width;
-	YSelection = aIndex / Width;
+	LabelFont->PutString(aItem->GetText().c_str(), aX, aY, aSelected ? 0xC08080FF : 0x808080FF);
+
+	return false;
 }
 
-bool										SummerfaceGrid::WasCanceled							()
+bool										SummerfaceLineList::Draw							()
 {
-	return Canceled;
+	if(Items.size() != 0)
+	{
+		uint32_t itemheight = LabelFont->GetHeight();
+		LinesDrawn = es_video->GetClip().Height / itemheight;
+		
+		//TODO: Fix it to draw one or two line lists!
+		if(LinesDrawn < 3)
+		{
+			return true;
+		}
+		
+		uint32_t online = 0;
+	
+		for(int i = SelectedIndex - LinesDrawn / 2; i != SelectedIndex + LinesDrawn / 2; i ++)
+		{
+			if(i < 0)
+			{
+				online ++;
+				continue;
+			}
+		
+			if(i >= Items.size())
+			{
+				break;
+			}
+			
+
+			if(DrawItem(Items[i], 16, (online * itemheight), i == SelectedIndex))
+			{
+				return true;
+			}	
+
+			online ++;
+		}
+	}
+
+	return false;
+}
+
+bool										SummerfaceLineList::Input							()
+{
+	if(Items.size() != 0)
+	{
+		SelectedIndex += (es_input->ButtonPressed(0, ES_BUTTON_DOWN) ? 1 : 0);
+		SelectedIndex -= (es_input->ButtonPressed(0, ES_BUTTON_UP) ? 1 : 0);
+		SelectedIndex += (es_input->ButtonPressed(0, ES_BUTTON_RIGHT) ? LinesDrawn : 0);
+		SelectedIndex -= (es_input->ButtonPressed(0, ES_BUTTON_LEFT) ? LinesDrawn : 0);
+	
+		SelectedIndex = Utility::Clamp(SelectedIndex, 0, Items.size() - 1);
+	}
+
+	if(InputHandler)
+	{
+		return InputHandler->HandleInput(GetName(), Items[SelectedIndex]); 
+	}
+	else if(es_input->ButtonDown(0, ES_BUTTON_ACCEPT))
+	{
+		Canceled = false;
+		return true;
+	}
+
+	if(es_input->ButtonDown(0, ES_BUTTON_CANCEL))
+	{
+		Canceled = true;
+		return true;
+	}
+	
+	return false;
 }
 
 
@@ -182,6 +417,12 @@ std::string									SummerfaceItem::GetImage							()
 											Summerface::Summerface								()
 {
 }
+
+											Summerface::Summerface								(const std::string& aName, SummerfaceWindow* aWindow)
+{
+	AddWindow(aName, aWindow);
+}
+
 
 											Summerface::~Summerface								()
 {
@@ -225,6 +466,7 @@ void										Summerface::AddWindow								(const std::string& aName, Summerface
 {
 	if(!Windows[aName] && aWindow)
 	{
+		aWindow->SetInterface(this, aName);
 		Windows[aName] = aWindow;
 		ActiveWindow = aName;
 	}
--- system/src/utility/GUI/Summerface.h
+++ system/src/utility/GUI/Summerface.h
@@ -6,7 +6,7 @@ class	Summerface;
 class													SummerfaceWindow
 {
 	public:
-														SummerfaceWindow				(Summerface* aInterface, const Area& aRegion);
+														SummerfaceWindow				(const Area& aRegion);
 		virtual											~SummerfaceWindow				();
 
 		bool											PrepareDraw						();
@@ -14,10 +14,15 @@ class													SummerfaceWindow
 		virtual bool									Draw							() = 0;
 		virtual bool									Input							() = 0;
 
+		Summerface*										GetInterface					();
+		std::string										GetName							();
+		void											SetInterface					(Summerface* aInterface, const std::string& aName);
+
 	protected:
 		static const uint32_t							BorderWidth = 4;
 
 		Summerface*										Interface;
+		std::string										Name;
 		Area											Region;
 		Area											Client;
 };
@@ -34,45 +39,105 @@ class													SummerfaceItem
 		virtual std::string								GetText							();
 		virtual std::string								GetImage						();
 
+	public:
+		std::map<std::string, std::string>				Properties;
+
 	protected:
 		std::string										Text;
 		std::string										Image;
 };
 
-class													SummerfaceGrid : public SummerfaceWindow
+class													SummerfaceInputConduit
 {
 	public:
-														SummerfaceGrid					(Summerface* aInterface, const Area& aRegion, uint32_t aWidth, uint32_t aHeight);
-		virtual											~SummerfaceGrid					();
+		virtual bool									HandleInput						(const std::string& aWindow, SummerfaceItem* aItem) = 0;
+};
 
+class													SummerfaceStaticConduit	: public SummerfaceInputConduit
+{
+	public:
+														SummerfaceStaticConduit			(bool (*aCallback)(void*, const std::string&, SummerfaceItem*), void* aUserData);
 
-		virtual bool									DrawItem						(SummerfaceItem* aItem, uint32_t aX, uint32_t aY, uint32_t aWidth, uint32_t aHeight, bool aSelected);
-		virtual bool									Draw							();
-		virtual bool									Input							();
+		virtual bool									HandleInput						(const std::string& aWindow, SummerfaceItem* aItem);
+
+	protected:
+		bool											(*Callback)						(void*, const std::string&, SummerfaceItem*);
+		void*											UserData;
+};
+
+class													SummerfaceList : public SummerfaceWindow
+{
+	public:
+														SummerfaceList					(const Area& aRegion);
+		virtual											~SummerfaceList					();
+
+		virtual void									AddItem							(SummerfaceItem* aItem);
+
+		SummerfaceItem*									GetSelected						();
 
 		uint32_t										GetItemCount					();
 		void											SetSelection					(uint32_t aIndex);
 
 		bool											WasCanceled						();
 
+		void											SetInputConduit					(SummerfaceInputConduit* aInputConduit, bool aDelete);
+		SummerfaceInputConduit*							GetInputConduit					();
+
+		void											SetFont							(Font* aFont);
 
 	protected:
 		std::vector<SummerfaceItem*>					Items;
+		uint32_t										SelectedIndex;
+		bool											Canceled;
 
+		Font*											LabelFont;
+
+		SummerfaceInputConduit*							InputHandler;
+		bool											DeleteInputHandler;
+};
+
+class													SummerfaceGrid : public SummerfaceList
+{
+	public:
+														SummerfaceGrid					(const Area& aRegion, uint32_t aWidth, uint32_t aHeight, bool aHeader = true, bool aLabels = false);
+		virtual											~SummerfaceGrid					();
+
+		virtual bool									DrawItem						(SummerfaceItem* aItem, uint32_t aX, uint32_t aY, uint32_t aWidth, uint32_t aHeight, bool aSelected);
+		virtual bool									Draw							();
+		virtual bool									Input							();
+
+		void											SetDrawMode						(bool aHeader, bool aLabels);
+
+
+	protected:
 		uint32_t										Width;
 		uint32_t										Height;
 
-		bool											Canceled;
-
-		int32_t											XSelection;
-		int32_t											YSelection;
-		int32_t											SelectedIndex;
+		bool											DrawHeader;
+		bool											DrawLabels;
 };
+
+class													SummerfaceLineList : public SummerfaceList
+{
+	public:
+														SummerfaceLineList				(const Area& aRegion);
+		virtual											~SummerfaceLineList				();
+
+		virtual bool									DrawItem						(SummerfaceItem* aItem, uint32_t aX, uint32_t aY, bool aSelected);
+		virtual bool									Draw							();
+		virtual bool									Input							();
+
+	protected:
+		uint32_t										LinesDrawn;
+};
+
 
 class													Summerface : public Menu
 {
 	public:
 														Summerface						();
+														Summerface						(const std::string& aName, SummerfaceWindow* aWindow);
+
 		virtual											~Summerface						();
 
 		virtual bool									Input							();
