921f9729cd834e982a2db9d10fb254591dfe21ef
Mike <Mike@localhost>
2011-01-24 03:27:25 -0500

    Fix aspect of control layout images

--- system/src/utility/GUI/Summerface/SummerfaceImage.cpp
+++ system/src/utility/GUI/Summerface/SummerfaceImage.cpp
@@ -15,7 +15,12 @@ bool										SummerfaceImage::Draw								()
 {
 	if(ImageManager::GetImage(Image))
 	{
-		es_video->PlaceTexture(ImageManager::GetImage(Image), 1, 1, es_video->GetClip().Width - 2, es_video->GetClip().Height - 2, 0xFFFFFFFF);
+		Texture* tex = ImageManager::GetImage(Image);
+
+		uint32_t x = 1, y = 1, w = es_video->GetClip().Width - 2, h = es_video->GetClip().Height - 2;
+		Utility::CenterAndScale(x, y, w, h, tex->GetWidth(), tex->GetHeight());
+
+		es_video->PlaceTexture(tex, x, y, w, h, 0xFFFFFFFF);
 	}
 
 	return false;
