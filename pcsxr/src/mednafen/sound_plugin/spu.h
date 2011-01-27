45259561442ca933785017133b4345d28c94e0fa
Mike <Mike@localhost>
2011-01-26 20:19:55 -0500

    Check in new version of PCSX

---
+++ pcsxr/src/mednafen/sound_plugin/spu.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+                            spu.h  -  description
+                             -------------------
+    begin                : Wed May 15 2002
+    copyright            : (C) 2002 by Pete Bernert
+    email                : BlackDove@addcom.de
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version. See also the license.txt file for *
+ *   additional informations.                                              *
+ *                                                                         *
+ ***************************************************************************/
+
+//ROBO: All spu functions are now pkSPU
+
+void SetupTimer(void);
+void RemoveTimer(void);
+void CALLBACK pkSPUplayADPCMchannel(xa_decode_t *xap);
+void CALLBACK pkSPUplayCDDAchannel(short *pcm, int bytes);
