a0040448f733a4cf8a7a7a642a332dd99ab76f19
Mike <Mike@localhost>
2011-02-13 10:15:44 -0500

    Refactor image loading

--- system/src/utility/ImageManager.cpp
+++ system/src/utility/ImageManager.cpp
@@ -1,13 +1,88 @@
 #include <ps3_system.h>
 
+											ImageManager::PNGFile::PNGFile						(const std::string& aPath)
+{
+	char header[8];
+		
+	FILE *fp = fopen(aPath.c_str(), "rb");
+	if (!fp) Abort("[read_png_file] File could not be opened for reading");
+
+	fread(header, 1, 8, fp);
+	if (png_sig_cmp((png_bytep)header, 0, 8)) Abort("[read_png_file] File is not recognized as a PNG file");
+	
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png_ptr) Abort("[read_png_file] png_create_read_struct failed");
+	
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) Abort("[read_png_file] png_create_info_struct failed");
+	
+	if (setjmp(png_jmpbuf(png_ptr))) Abort("[read_png_file] Error during init_io");
+	
+	png_init_io(png_ptr, fp);
+	png_set_sig_bytes(png_ptr, 8);
+	
+	if (setjmp(png_jmpbuf(png_ptr))) Abort("[read_png_file] Error during read_image");
+	
+#ifdef L1GHT	
+	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_SWAP_ALPHA, 0);
+#else
+	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_BGR, 0);
+#endif
+	
+	fclose(fp);
+
+	row_pointers = png_get_rows(png_ptr, info_ptr);
+	Width = png_get_image_width(png_ptr, info_ptr);
+	Height = png_get_image_height(png_ptr, info_ptr);		
+}
+
+											ImageManager::PNGFile::~PNGFile						()
+{
+	png_destroy_info_struct(png_ptr, &info_ptr);
+	png_destroy_read_struct(&png_ptr, 0, 0);
+}
+
+void										ImageManager::PNGFile::CopyToTexture				(Texture* aTexture)
+{
+	if(info_ptr->color_type == PNG_COLOR_TYPE_RGB_ALPHA)
+	{
+		for(int i = 0; i != Height; i ++)
+		{
+			memcpy(aTexture->GetPixels() + (aTexture->GetWidth() * i), row_pointers[i], Width * 4);
+		}
+	}
+	else
+	{
+		for(int i = 0; i != Height; i ++)
+		{
+			uint32_t* dest = aTexture->GetPixels() + (aTexture->GetWidth() * i);
+			uint8_t* source = row_pointers[i];
+
+			for(int j = 0; j != Width; j ++)
+			{
+				uint32_t r = *source ++;
+				uint32_t g = *source ++;
+				uint32_t b = *source ++;
+				*dest++ = (r << 0) | (g << 8) | (b << 16) | 0xFF000000;
+			}
+		}
+	}
+}
+
 void										ImageManager::Purge									()
 {
 	for(std::map<std::string, Texture*>::iterator i = Images.begin(); i != Images.end(); i ++)
 	{
 		delete i->second;
 	}
+
+	for(std::map<std::string, PNGFile*>::iterator i = Files.begin(); i != Files.end(); i ++)
+	{
+		delete i->second;
+	}
 	
 	Images.clear();
+	Files.clear();
 }
 
 void										ImageManager::LoadDirectory							(const std::string& aPath)
@@ -50,74 +125,12 @@ Texture*									ImageManager::LoadImage								(const std::string& aName, const
 {
 	if(Images[aName] == 0)
 	{
-		char header[8];
-		
-		FILE *fp = fopen(aPath.c_str(), "rb");
-		if (!fp)
-			Abort("[read_png_file] File could not be opened for reading");
-		fread(header, 1, 8, fp);
-			if (png_sig_cmp((png_bytep)header, 0, 8))
-				Abort("[read_png_file] File is not recognized as a PNG file");
-	
-		png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-		if (!png_ptr)
-			Abort("[read_png_file] png_create_read_struct failed");
-	
-		png_infop info_ptr = png_create_info_struct(png_ptr);
-		if (!info_ptr)
-			Abort("[read_png_file] png_create_info_struct failed");
-	
-		if (setjmp(png_jmpbuf(png_ptr)))
-			Abort("[read_png_file] Error during init_io");
-	
-		png_init_io(png_ptr, fp);
-		png_set_sig_bytes(png_ptr, 8);
-	
-		if (setjmp(png_jmpbuf(png_ptr)))
-			Abort("[read_png_file] Error during read_image");
+		PNGFile ping(aPath);
 
-#ifdef L1GHT	
-		png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_SWAP_ALPHA, 0);
-#else
-		png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_BGR, 0);
-#endif
-	
-		fclose(fp);
-
-		png_bytep* row_pointers = png_get_rows(png_ptr, info_ptr);
-		png_uint_32 width = png_get_image_width(png_ptr, info_ptr);
-		png_uint_32 height = png_get_image_height(png_ptr, info_ptr);		
-
-		Texture* output = es_video->CreateTexture(width, height);
+		Texture* output = es_video->CreateTexture(ping.Width, ping.Height);
 		output->SetFilter(true);
 
-		if(info_ptr->color_type == PNG_COLOR_TYPE_RGB_ALPHA)
-		{
-			for(int i = 0; i != height; i ++)
-			{
-				memcpy(output->GetPixels() + (output->GetWidth() * i), row_pointers[i], width * 4);
-			}
-		}
-		else
-		{
-			for(int i = 0; i != height; i ++)
-			{
-				uint32_t* dest = output->GetPixels() + (output->GetWidth() * i);
-				uint8_t* source = row_pointers[i];
-
-				for(int j = 0; j != width; j ++)
-				{
-					uint32_t r = *source ++;
-					uint32_t g = *source ++;
-					uint32_t b = *source ++;
-					*dest++ = (r << 0) | (g << 8) | (b << 16) | 0xFF000000;
-				}
-			}
-		}
-
-		
-		png_destroy_info_struct(png_ptr, &info_ptr);
-		png_destroy_read_struct(&png_ptr, 0, 0);
+		ping.CopyToTexture(output);
 		
 		Images[aName] = output;
 	}
@@ -130,5 +143,32 @@ Texture*									ImageManager::GetImage								(const std::string& aName)
 	return Images[aName];
 }
 
+void										ImageManager::FillScratch							(uint32_t aScratchIndex, const std::string& aFileName)
+{
+	if(aScratchIndex >= 20)
+	{
+		throw ESException("FillScratch: Index out of range [Index: %d]", aScratchIndex);
+	}
+	
+	char buffer[256];
+	snprintf(buffer, 256, "SCRATCH%%%d", aScratchIndex);
+	Texture* scratch = GetImage(buffer);
+	
+	if(!scratch)
+	{
+		throw ESException("FillScratch: Scratch asset not available [Index: %d]", aScratchIndex);
+	}
+	
+	if(Files[aFileName] == 0)
+	{
+		//Load it
+		PNGFile* file = new PNGFile(aFileName);
+		Files[aFileName] = file;
+	}
+	
+	Files[aFileName]->CopyToTexture(scratch);
+}
+
 
 std::map<std::string, Texture*>				ImageManager::Images;
+std::map<std::string, ImageManager::PNGFile*>ImageManager::Files;
