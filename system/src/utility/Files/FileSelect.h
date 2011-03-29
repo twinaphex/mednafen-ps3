d20191e58d65e86857656d45f2160953bbdffe0a
Mike <Mike@localhost>
2011-03-29 13:38:28 -0400

    Fix bookmarks

--- system/src/utility/Files/FileSelect.h
+++ system/src/utility/Files/FileSelect.h
@@ -1,21 +1,24 @@
 #ifndef SYSTEM__FILESELECT_H
 #define SYSTEM__FILESELECT_H
 
+typedef std::vector<std::string>					BookmarkList;
+
 class												FileSelect
 {
 	public:
-													FileSelect						(const std::string& aHeader, std::vector<std::string>& aBookMarks, const std::string& aPath, MenuHook* aInputHook = 0);
+													FileSelect						(const std::string& aHeader, BookmarkList& aBookMarks, const std::string& aPath, MenuHook* aInputHook = 0);
 		virtual										~FileSelect						() {};
 													
+		bool										HandleInput						(Summerface_Ptr aInterface, const std::string& aWindow);
+
 		std::string									GetFile							();
 		bool										IsValid							() {return Valid;}
 
 	private:
-		void										LoadList						(const char* aPath);
-		static SummerfaceItem_Ptr					MakeItem						(const std::string& aName, const std::string& aPath, bool aDirectory, bool aFile, bool aBookMark);
+		void										LoadList						(const std::string& aPath);
+		SummerfaceItem_Ptr							MakeItem						(const std::string& aName, const std::string& aPath, bool aDirectory, bool aFile);
 
 	private:
-
 		SummerfaceList_Ptr							List;
 		Summerface_Ptr								Interface;
 		std::stack<std::string>						Paths;
@@ -23,7 +26,7 @@ class												FileSelect
 		bool										Valid;
 
 		std::string									Header;
-		std::vector<std::string>&					BookMarks;
+		BookmarkList&								BookMarks;
 };
 
 #endif
