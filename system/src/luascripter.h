45713bb93331012edb1fbd643c3cf58eb0169ae5
Mike <Mike@localhost>
2011-06-27 20:24:47 -0400

    Remove unused XML routines from LuaScripter

--- system/src/luascripter.h
+++ system/src/luascripter.h
@@ -20,12 +20,6 @@ class								LuaScripter
 		uint32_t					Call							(const std::string& aFunction, uint32_t aA);
 		uint32_t					Call							(const std::string& aFunction, uint32_t aA, uint32_t aB);
 
-//		void						InsertDataSetItem				(const TiXmlNode* aDescription, const TiXmlNode* aData);
-//		void						InsertDataSet					(const std::string& aFileName);
-
-//		const TiXmlNode*			InsertData						(uint32_t aID, const std::string& aFileName, const std::string& aName);
-//		void						DeleteData						(uint32_t aID);
-
 	public: //HACK
 
 		lua_State*					LuaState;
