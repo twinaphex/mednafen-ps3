785bb28c9ef2f58a33fd40aaf00221fb06800474
Mike <Mike@localhost>
2011-07-07 02:18:04 -0400

    Check in pcsx with libpcsxcore revision 68249 (update plugins later)

---
+++ pcsxr/src/mednafen/input_plugin/pad.h
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2009, Wei Mingzhi <whistler@openoffice.org>.
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
+#ifndef PAD_H_
+#define PAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "psemu_plugin_defs.h"
+
+enum {
+	DKEY_SELECT = 0,
+	DKEY_L3,
+	DKEY_R3,
+	DKEY_START,
+	DKEY_UP,
+	DKEY_RIGHT,
+	DKEY_DOWN,
+	DKEY_LEFT,
+	DKEY_L2,
+	DKEY_R2,
+	DKEY_L1,
+	DKEY_R1,
+	DKEY_TRIANGLE,
+	DKEY_CIRCLE,
+	DKEY_CROSS,
+	DKEY_SQUARE,
+
+	DKEY_TOTAL
+};
+
+enum {
+	ANALOG_LEFT = 0,
+	ANALOG_RIGHT,
+
+	ANALOG_TOTAL
+};
+
+enum { NONE = 0, AXIS, HAT, BUTTON };
+
+typedef struct tagKeyDef {
+	uint8_t			JoyEvType;
+	union {
+		int16_t		d;
+		int16_t		Axis;   // positive=axis+, negative=axis-, abs(Axis)-1=axis index
+		uint16_t	Hat;	// 8-bit for hat number, 8-bit for direction
+		uint16_t	Button; // button number
+	} J;
+	uint16_t		Key;
+} KEYDEF;
+
+enum { ANALOG_XP = 0, ANALOG_XM, ANALOG_YP, ANALOG_YM };
+
+typedef struct tagPadDef {
+	int8_t			DevNum;
+	uint16_t		Type;
+	KEYDEF			KeyDef[DKEY_TOTAL];
+	KEYDEF			AnalogDef[ANALOG_TOTAL][4];
+} PADDEF;
+
+typedef struct tagConfig {
+	uint8_t			Threaded;
+	PADDEF			PadDef[2];
+} CONFIG;
+
+typedef struct tagPadState {
+	uint8_t				PadMode;
+	uint8_t				PadID;
+	volatile uint16_t	KeyStatus;
+	volatile uint16_t	JoyKeyStatus;
+	volatile uint8_t	AnalogStatus[ANALOG_TOTAL][2]; // 0-255 where 127 is center position
+	volatile uint8_t	AnalogKeyStatus[ANALOG_TOTAL][4];
+} PADSTATE;
+
+typedef struct tagGlobalData {
+	CONFIG				cfg;
+
+	uint8_t				Opened;
+	void				*Disp; //Unused now
+
+	PADSTATE			PadState[2];
+	volatile long		KeyLeftOver;
+} GLOBALDATA;
+
+extern GLOBALDATA		g;
+
+enum {
+	CMD_READ_DATA_AND_VIBRATE = 0x42,
+	CMD_CONFIG_MODE = 0x43,
+	CMD_SET_MODE_AND_LOCK = 0x44,
+	CMD_QUERY_MODEL_AND_MODE = 0x45,
+	CMD_QUERY_ACT = 0x46, // ??
+	CMD_QUERY_COMB = 0x47, // ??
+	CMD_QUERY_MODE = 0x4C, // QUERY_MODE ??
+	CMD_VIBRATION_TOGGLE = 0x4D,
+};
+
+// cfg.c functions...
+void LoadPADConfig();
+void SavePADConfig();
+
+// analog.c functions...
+void InitAnalog();
+void CheckAnalog();
+int AnalogKeyPressed(uint16_t Key);
+int AnalogKeyReleased(uint16_t Key);
+
+// pad.c functions...
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ pcsxr/src/mednafen/main.c
@@ -0,0 +1,139 @@
+#include "psxcommon.h"
+#include "plugins.h"
+#include "video_plugin/globals.h"
+#include "input_plugin/pad.h"
+
+//C++ wrapper functions
+const char*			GetMemoryCardName		(int aSlot);		//Get the Memory card filename
+const char*			GetBiosName				();					//Get the bios filename
+uint32_t			GetRecompiler			();					//Get the recompiler setting
+
+//Plugins functions, from plugins.c
+int					OpenPlugins		();
+void				ClosePlugins	();
+
+//System functions, needed by libpcsxcore
+//Setting wanna_leave to 1 causes the cpu emu to return, but this is a patch by me
+extern int			wanna_leave;
+void				SysUpdate		()		{wanna_leave = 1;}
+void				SysRunGui		()		{/* Nothing */}
+
+void				SysReset		()
+{
+	//Reset the emulator
+	EmuReset();
+}
+
+void				SysClose		()
+{
+	//Free all memory allocated by the emulator
+	EmuShutdown();
+
+	//Calls the close function for all of the plugins, then calls libpcsxcore's ReleasePlugins to shut them down
+	ClosePlugins();
+}
+
+//Main functions for running emu
+int					SysLoad			()
+{
+	//Setup the config structure
+    memset(&Config, 0, sizeof(Config));
+    Config.PsxAuto = 1;
+    Config.Cpu = GetRecompiler() ? CPU_DYNAREC : CPU_INTERPRETER;
+    strcpy(Config.PluginsDir, "builtin");
+    strcpy(Config.Gpu, "builtin");
+    strcpy(Config.Spu, "builtin");
+    strcpy(Config.Pad1, "builtin");
+    strcpy(Config.Pad2, "builtin");
+    strcpy(Config.Cdr, "builtin");
+    strcpy(Config.Net, "Disabled");
+    strncpy(Config.Bios, GetBiosName(), MAXPATHLEN);
+	strncpy(Config.Mcd1, GetMemoryCardName(0), MAXPATHLEN);
+	strncpy(Config.Mcd2, GetMemoryCardName(1), MAXPATHLEN);
+
+	//Init psx cpu emulator and memory mapping
+	EmuInit();
+
+	//Open and initialize all of the plugins
+	OpenPlugins();
+
+	//Load memory cards
+	LoadMcds(Config.Mcd1, Config.Mcd2);
+
+	//Get cdrom ID and init PPF support...
+	CheckCdrom();
+
+	//Reset the emulated CPU and Memory
+	EmuReset();
+
+	//Prepares the game to run, either runs the CPU thru the bios or finds and loads the game EXE if using the emulated bios
+	LoadCdrom();
+}
+
+//external sound varibles, move em later
+extern uint8_t SoundBuf[48000];
+extern uint32_t SoundBufLen;
+
+#define RED(x) (x & 0xff)
+#define BLUE(x) ((x>>16) & 0xff)
+#define GREEN(x) ((x>>8) & 0xff)
+#define COLOR(x) (x & 0xffffff)
+
+void		SysFrame			(uint32_t aSkip, uint32_t* aPixels, uint32_t aPitch, uint32_t aKeys, uint32_t* aWidth, uint32_t* aHeight, uint32_t* aSound, uint32_t* aSoundLen)
+{
+	//Send input
+	g.PadState[0].JoyKeyStatus = ~aKeys;
+	g.PadState[0].KeyStatus = ~aKeys;
+
+	//Run frame
+	psxCpu->Execute();
+
+	//Grab the frame
+	int x = 0, y = 0, w = 320, h = 240;
+
+	if(!aSkip && g_gpu.dsp.mode.x)
+	{
+		x = g_gpu.dsp.position.x;
+	    y = g_gpu.dsp.position.y;
+	    w = (g_gpu.dsp.range.x1 - g_gpu.dsp.range.x0) / g_gpu.dsp.mode.x;
+	    h = (g_gpu.dsp.range.y1 - g_gpu.dsp.range.y0) * g_gpu.dsp.mode.y;
+
+		if(g_gpu.status_reg & STATUS_RGB24)
+		{
+			for(int i = 0; i != h; i++)
+			{
+				int startxy = ((1024) * (i + y)) + x;
+				unsigned char* pD = (unsigned char *)&g_gpu.psx_vram.u16[startxy];
+				uint32_t* destpix = (uint32_t *)(aPixels + (i * aPitch));
+				for(int j = 0; j != w; j++)
+				{
+					uint32_t lu = SWAP32(*((uint32_t *)pD));
+					destpix[j] = 0xff000000 | (RED(lu) << 16) | (GREEN(lu) << 8) | (BLUE(lu));
+					pD += 3;
+				}
+			}
+		}
+		else
+		{
+			for(int i = 0; i != h; i++)
+			{
+				int startxy = (1024 * (i + y)) + x;
+				uint32_t* destpix = &aPixels[aPitch * i];
+				for(int j = 0; j != w; j++, startxy ++)
+				{
+					int s = SWAP16(g_gpu.psx_vram.u16[startxy]);
+					destpix[j] = (((s << 19) & 0xf80000) | ((s << 6) & 0xf800) | ((s >> 7) & 0xf8)) | 0xff000000;
+				}
+			}
+		}
+	}
+
+	*aWidth = w;
+	*aHeight = h;
+
+	//Grab the audio
+	memcpy(aSound, SoundBuf, SoundBufLen);
+	*aSoundLen = SoundBufLen / 4;
+	SoundBufLen = 0;
+}
+
