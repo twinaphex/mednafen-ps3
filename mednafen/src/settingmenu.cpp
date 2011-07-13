68a03fe58632f7c93ffeac9e2c430995c0e25b23
Mike <Mike@localhost>
2011-07-13 00:59:31 -0400

    Bug fixes and tweaks for the medanfen setting menu

--- mednafen/src/settingmenu.cpp
+++ mednafen/src/settingmenu.cpp
@@ -21,138 +21,32 @@ static bool						CompareItems									(SummerfaceItem_Ptr a, SummerfaceItem_Ptr 
 	return a->GetText() < b->GetText();
 }
 
-void							SettingLineView::DoHeaderRefresh				()
-{
-	if(!WeakList.expired() && RefreshHeader)
-	{
-		RefreshHeader = false;
-
-		SummerfaceList_Ptr List = WeakList.lock();
-
-		const MDFNCS& Setting = *(const MDFNCS*)List->GetSelected()->IntProperties["MDFNCS"];
-		List->SetHeader(Setting.desc->description);
-	}
-}
-
-bool							SettingLineView::DrawItem						(SummerfaceList_Ptr aList, SummerfaceItem_Ptr aItem, uint32_t aX, uint32_t aY, bool aSelected)
-{
-	AnchoredListView::DrawItem(aList, aItem, aX, aY, aSelected);
-
-	const MDFNCS* setting = (const MDFNCS*)aItem->IntProperties["MDFNCS"];
-
-	if(setting)
-	{
-		char buffer[256];
-		snprintf(buffer, 250, "%s", (setting->desc->type == MDFNST_BOOL) ? (MDFN_GetSettingB(setting->name) ? "ON" : "OFF") : MDFN_GetSettingS(setting->name).c_str());
-
-//HACK: Use real font
-	  //  List->LabelFont->PutString(buffer, aX + (ESVideo::GetClip().Width / 3) * 2, aY, aSelected ? aItem->GetHighLightColor() : aItem->GetNormalColor());
-		FontManager::GetBigFont()->PutString(buffer, aX + (ESVideo::GetClip().Width / 3) * 2, aY, aSelected ? aItem->GetHighLightColor() : aItem->GetNormalColor(), true);
-
-	}
-	return false;
-}
-
-bool							SettingLineView::HandleBool						(uint32_t aButton, const MDFNCS& aSetting)
-{
-	if(aButton == ES_BUTTON_LEFT || aButton == ES_BUTTON_RIGHT || aButton == ES_BUTTON_ACCEPT)
-	{
-		MDFNI_SetSettingB(aSetting.name, MDFN_GetSettingB(aSetting.name) == 0);
-		MednafenEmu::ReadSettings();
-		return true;
-	}
-
-	return aButton == ES_BUTTON_TAB; //Using file browser for a bool is nonsense;
-}
-
-bool							SettingLineView::HandleEnum						(uint32_t aButton, const MDFNCS& aSetting)
-{
-	const MDFNSetting_EnumList* values = aSetting.desc->enum_list;
-
-	if(aButton == ES_BUTTON_LEFT || aButton == ES_BUTTON_RIGHT) //Cycle values
-	{
-		int32_t value = (aButton == ES_BUTTON_LEFT) ? -1 : 0;
-		value = (aButton == ES_BUTTON_RIGHT) ? 1 : value;
-		std::string oldvalue = MDFN_GetSettingS(aSetting.name);
-
-		while(values && values->string && strcmp(values->string, oldvalue.c_str()))
-		{
-			values ++;
-		}
-
-		if(values && values->string && !(values == aSetting.desc->enum_list && value < 0))
-		{
-			values += value;
-
-			if(values->string)
-			{
-				MDFNI_SetSetting(aSetting.name, values->string);
-				MednafenEmu::ReadSettings();
-
-				return true;
-			}
-		}
-	}
-	else if(aButton == ES_BUTTON_ACCEPT) //Choose from list
-	{
-		SummerfaceList_Ptr list = smartptr::make_shared<SummerfaceList>(Area(10, 10, 80, 80));
-		list->SetView(smartptr::make_shared<AnchoredListView>(list));
-		while(values && values->string)
-		{
-			SummerfaceItem_Ptr item = smartptr::make_shared<SummerfaceItem>(values->string, "");
-			list->AddItem(item);
-			values ++;
-		}
-
-		Summerface::Create("List", list)->Do();
-
-		if(!list->WasCanceled())
-		{
-			MDFNI_SetSetting(aSetting.name, list->GetSelected()->GetText().c_str());
-			MednafenEmu::ReadSettings();
-		}
-
-		return true;
-	}
-
-	return aButton == ES_BUTTON_TAB; //Using file browser for an enumeration is nonsense
-}
-
-bool							SettingLineView::HandleInt						(uint32_t aButton, const MDFNCS& aSetting)
-{
-	int32_t value = (aButton == ES_BUTTON_LEFT) ? -1 : 0;
-	value = (aButton == ES_BUTTON_RIGHT) ? 1 : value;
-
-	if(value != 0)
-	{
-		if(aSetting.desc->type == MDFNST_UINT)
-		{
-			MDFNI_SetSettingUI(aSetting.name, MDFN_GetSettingUI(aSetting.name) + value);
-		}
-		else
-		{
-			char buffer[256];
-			snprintf(buffer, 256, "%lli", (long long int)MDFN_GetSettingI(aSetting.name) + value);
-			MDFNI_SetSetting(aSetting.name, buffer);
-		}
-
-		MednafenEmu::ReadSettings();
-		return true;
-	}
-
-	return aButton == ES_BUTTON_TAB; //Using file browser for an int is nonsense
-}
-
 bool							SettingLineView::Input							(uint32_t aButton)
 {
 	if(!WeakList.expired())
 	{
+		//Get a pointer to the list
 		SummerfaceList_Ptr List = WeakList.lock();
 
+		//Leave if there is no list item
+		if(!List->GetSelected() || !List->GetSelected()->IntProperties["MDFNCS"] || !List->GetInterface())
+		{
+			return false;
+		}
+
+		//Get a refence to the setting
 		const MDFNCS& Setting = *(const MDFNCS*)List->GetSelected()->IntProperties["MDFNCS"];
 
-		DoHeaderRefresh();
+		//HACK: Set the input wait flag if no header refresh is waiting
+		if(RefreshHeader == false && List->GetInterface())
+		{
+			List->GetInterface()->SetInputWait(true);
+		}
 
+		//Refresh the header
+		DoHeaderRefresh(List);
+
+		//Check for and handle setting types
 		if(Setting.desc->type == MDFNST_BOOL && HandleBool(aButton, Setting))
 		{
 			return false;
@@ -166,24 +60,29 @@ bool							SettingLineView::Input							(uint32_t aButton)
 			return false;
 		}
 
-		if(aButton == ES_BUTTON_ACCEPT) //Use Keyboard
+		//Fire up the keyboard (may be canceled by setting handlers)
+		if(aButton == ES_BUTTON_ACCEPT)
 		{
+			//TODO: Error Checking?
 			std::string result = ESSUB_GetString(Setting.name, MDFN_GetSettingS(Setting.name));
 			MDFNI_SetSetting(Setting.name, result.c_str());
+			MednafenEmu::ReadSettings();
 			return false;
 		}
-		else if(aButton == ES_BUTTON_SHIFT) //Reset to default
+		//Set setting back to its default
+		else if(aButton == ES_BUTTON_SHIFT)
 		{
 			MDFNI_SetSetting(Setting.name, Setting.desc->default_value);
+			MednafenEmu::ReadSettings();
 			return false;
 		}
-		else if(aButton == ES_BUTTON_TAB) //Select from file browser
+		//Browse for a file and use its path as the setting value (may be canceled by setting handlers)
+		else if(aButton == ES_BUTTON_TAB)
 		{
 			std::vector<std::string> nomarks;
 			FileSelect browse("Select File", nomarks, "");
 			
 			std::string result = browse.GetFile();
-			
 			if(result.length() != 0)
 			{
 				MDFNI_SetSetting(Setting.name, result.c_str());
@@ -193,25 +92,178 @@ bool							SettingLineView::Input							(uint32_t aButton)
 			return false;
 		}
 
-		if(!aButton == ES_BUTTON_LEFT && !aButton == ES_BUTTON_RIGHT) //Don't allow underlying list view to use left and right
+		//If the button isn't left or right pass it to the AnchoredListView class.
+		if(aButton != ES_BUTTON_LEFT && aButton != ES_BUTTON_RIGHT)
 		{
+			//Cache the selected item
 			SummerfaceItem_Ptr selected = List->GetSelected();
-			bool output = AnchoredListView::Input(aButton);
 
-			if(selected != List->GetSelected())
+			//Pass it to the base
+			if(!AnchoredListView::Input(aButton))
 			{
-				RefreshHeader = true;
-				DoHeaderRefresh();
-			}
+				//Schedule a header update if needed
+				if(selected != List->GetSelected())
+				{
+					RefreshHeader = true;
+					DoHeaderRefresh(List);
+				}
 
-			return output;
+				return false;
+			}
+			//The base list view said to exit
+			else
+			{
+				return true;
+			}
+		}
+	}
+
+	//Default return value
+	return false;
+}
+
+void							SettingLineView::DoHeaderRefresh				(SummerfaceList_Ptr aList)
+{
+	//If a refresh is scheduled and a list item is available
+	if(RefreshHeader && aList && aList->GetSelected() && aList->GetSelected()->IntProperties["MDFNCS"])
+	{
+		//Set the header
+		const MDFNCS& Setting = *(const MDFNCS*)aList->GetSelected()->IntProperties["MDFNCS"];
+		aList->SetHeader(Setting.desc->description);
+	}
+}
+
+bool							SettingLineView::DrawItem						(SummerfaceList_Ptr aList, SummerfaceItem_Ptr aItem, uint32_t aX, uint32_t aY, bool aSelected)
+{
+	//Tell the base class to do its part
+	AnchoredListView::DrawItem(aList, aItem, aX, aY, aSelected);
+
+	//Get a pointer to the setting and, if valid, draw it
+	const MDFNCS* setting = (const MDFNCS*)aItem->IntProperties["MDFNCS"];
+	if(setting)
+	{
+		//Draw using special casing for bools
+		aList->GetFont()->PutString((setting->desc->type == MDFNST_BOOL) ? (MDFN_GetSettingB(setting->name) ? "ON" : "OFF") : MDFN_GetSettingS(setting->name).c_str(), aX + (ESVideo::GetClip().Width / 3) * 2, aY, aSelected ? aItem->GetHighLightColor() : aItem->GetNormalColor(), true);
+	}
+
+	return false;
+}
+
+bool							SettingLineView::HandleBool						(uint32_t aButton, const MDFNCS& aSetting)
+{
+	//Toggle if the button is left, right, or accept
+	if(aButton == ES_BUTTON_LEFT || aButton == ES_BUTTON_RIGHT || aButton == ES_BUTTON_ACCEPT)
+	{
+		MDFNI_SetSettingB(aSetting.name, MDFN_GetSettingB(aSetting.name) == 0);
+		MednafenEmu::ReadSettings();
+		return true;
+	}
+
+	//Block using the file browser and keyboard to set a bool value
+	return aButton == ES_BUTTON_TAB || aButton == ES_BUTTON_ACCEPT;
+}
+
+bool							SettingLineView::HandleInt						(uint32_t aButton, const MDFNCS& aSetting)
+{
+	//Get the change to be applied
+	int32_t value = (aButton == ES_BUTTON_LEFT) ? -1 : 0;
+	value = (aButton == ES_BUTTON_RIGHT) ? 1 : value;
+
+	//Handle any changes
+	if(value != 0)
+	{
+		//Handle differences between UINT and INT settings
+		if(aSetting.desc->type == MDFNST_UINT)
+		{
+			MDFNI_SetSettingUI(aSetting.name, MDFN_GetSettingUI(aSetting.name) + value);
 		}
 		else
 		{
-			return false;
+			char buffer[256];
+			snprintf(buffer, 256, "%lli", (long long int)MDFN_GetSettingI(aSetting.name) + value);
+			MDFNI_SetSetting(aSetting.name, buffer);
+		}
+
+		//Tell the emulator to refresh then leave
+		MednafenEmu::ReadSettings();
+		return true;
+	}
+
+	//Block using the file browser to set an int value
+	return aButton == ES_BUTTON_TAB;
+}
+
+bool							SettingLineView::HandleEnum						(uint32_t aButton, const MDFNCS& aSetting)
+{
+	//Get a poitner to the enumeration values
+	const MDFNSetting_EnumList* values = aSetting.desc->enum_list;
+
+	//Cycle values with left and right
+	if(aButton == ES_BUTTON_LEFT || aButton == ES_BUTTON_RIGHT)
+	{
+		//Get the direction to move
+		int32_t value = (aButton == ES_BUTTON_LEFT) ? -1 : 0;
+		value = (aButton == ES_BUTTON_RIGHT) ? 1 : value;
+
+		//Get the settings current value
+		std::string oldvalue = MDFN_GetSettingS(aSetting.name);
+
+		//Advance the values pointer to the selected item
+		while(values && values->string && strcmp(values->string, oldvalue.c_str()))
+		{
+			values ++;
+		}
+
+		//Block moving before the first value
+		if(values && values->string && !(values == aSetting.desc->enum_list && value < 0))
+		{
+			//Update the values
+			values += value;
+
+			//Block moving past the last value
+			if(values->string)
+			{
+				//Update and leave
+				MDFNI_SetSetting(aSetting.name, values->string);
+				MednafenEmu::ReadSettings();
+
+				return true;
+			}
 		}
 	}
+	//Choose from list with accept
+	else if(aButton == ES_BUTTON_ACCEPT)
+	{
+		//Create the list
+		SummerfaceList_Ptr list = smartptr::make_shared<SummerfaceList>(Area(10, 10, 80, 80));
+		list->SetView(smartptr::make_shared<AnchoredListView>(list));
+		list->SetHeader(std::string("Choose ") + aSetting.name + "'s new value:");
+
+		//Place all settings into the list
+		while(values && values->string)
+		{
+			list->AddItem(smartptr::make_shared<SummerfaceItem>(values->string, ""));
+			values ++;
+		}
+
+		//Run the list
+		Summerface::Create("List", list)->Do();
+
+		//Get the new setting and feed it to mednafen
+		if(!list->WasCanceled())
+		{
+			MDFNI_SetSetting(aSetting.name, list->GetSelected()->GetText().c_str());
+			MednafenEmu::ReadSettings();
+		}
+
+		//Done
+		return true;
+	}
+
+	//Block using the file browser to set an int value
+	return aButton == ES_BUTTON_TAB;
 }
+
 
 
 								SettingMenu::SettingMenu						(const std::string& aDefaultCategory) :
@@ -219,25 +271,34 @@ bool							SettingLineView::Input							(uint32_t aButton)
 	CategoryList(smartptr::make_shared<SummerfaceList>(Area(10, 10, 80, 80))),
 	Interface(Summerface::Create("Categories", CategoryList))
 {
-	CategoryList->SetView(smartptr::make_shared<AnchoredListView>(CategoryList));
-	CategoryList->SetHeader("Choose Setting Category");
-	List->SetView(smartptr::make_shared<SettingLineView>(List));
-
+	//Cache the setting values from mednafen
 	LoadSettings();
 
+	//Setup the category list
+	CategoryList->SetView(smartptr::make_shared<AnchoredListView>(CategoryList));
+	CategoryList->SetHeader("Choose Setting Category");
+
+	//Setup the setting list
+	List->SetView(smartptr::make_shared<SettingLineView>(List));
+
+	//Stuff the category list
 	for(SettingCollection::iterator i = Settings.begin(); i != Settings.end(); i ++)
 	{
+		//Create the item
 		SummerfaceItem_Ptr item = smartptr::make_shared<SummerfaceItem>(TranslateCategory(i->first.c_str()), "");
 		item->Properties["CATEGORY"] = i->first;
 
+		//Highlight it if it is the default category
 		if(item->Properties["CATEGORY"] == aDefaultCategory)
 		{
 			item->SetColors(Colors::SpecialNormal, Colors::SpecialHighLight);
 		}
 
+		//Stash it in the list
 		CategoryList->AddItem(item);
 	}
 
+	//Sort the list and choose the default selection
 	CategoryList->Sort();
 	CategoryList->SetSelection(TranslateCategory(aDefaultCategory.c_str()));
 }
@@ -246,16 +307,16 @@ void							SettingMenu::Do									()
 {
 	while(!WantToDie())
 	{
+		//Run the category list
 		Interface->Do();
 
-		if(CategoryList->WasCanceled())
+		//Leave if the category list is canceld and everything checks out
+		if(!CategoryList->WasCanceled() && CategoryList->GetSelected() && !CategoryList->GetSelected()->Properties["CATEGORY"].empty())
 		{
-			return;
-		}
-		else
-		{
+			//Clear the setting list
 			List->ClearItems();
 
+			//Add all settings from the catagory
 			std::vector<const MDFNCS*> items = Settings[CategoryList->GetSelected()->Properties["CATEGORY"]];
 			for(int i = 0; i != items.size(); i ++)
 			{
@@ -264,19 +325,31 @@ void							SettingMenu::Do									()
 				List->AddItem(item);
 			}
 
+			//Sort the setting list
 			List->Sort(CompareItems);
 
-			Summerface::Create("SettingList", List)->Do();
+			//HACK: Create the interface without input wait until the header is update
+			Summerface_Ptr sface = Summerface::Create("SettingList", List);
+			sface->SetInputWait(false);
+			sface->Do();
+		}
+		else
+		{
+			//Done
+			return;
 		}
 	}
 }
 
 void							SettingMenu::LoadSettings						()
 {
+	//Get the settings map from mednafen
 	const std::multimap<uint32_t, MDFNCS>* settings = MDFNI_GetSettings();
 
+	//Iterate it and get all of the settings
 	for(std::multimap<uint32, MDFNCS>::const_iterator iter = settings->begin(); iter != settings->end(); iter++)
 	{
+		//Default category is "General Settings"
 		std::string header = "general";
 	
 		//HACK: Don't use filesys.*samedir
@@ -285,23 +358,27 @@ void							SettingMenu::LoadSettings						()
 			MDFNI_SetSettingB(iter->second.name, false);
 			continue;
 		}
-	
+
+		//Don't list input or the bookmarks settings, these are set in a different manner	
 		if(std::string(iter->second.name).find(".esinput.") != std::string::npos || std::string(iter->second.name).find(".bookmarks") != std::string::npos)
 		{
 			continue;
 		}
 	
+		//Get the header, all text leading up to the first dot
 		if(std::string(iter->second.name).find(".") != std::string::npos)
 		{
 			header = std::string(iter->second.name).substr(0, std::string(iter->second.name).find_first_of("."));
 		}
 
+		//Add the setting into the local cache
 		Settings[header].push_back(&iter->second);
 	}
 }
 
 std::string						SettingMenu::TranslateCategory					(const char* aCategoryName)
 {
+	//The name of any emulator module is the module's full name
 	for(int i = 0; i != MDFNSystems.size(); i ++)
 	{
 		if(strcmp(aCategoryName, MDFNSystems[i]->shortname) == 0)
@@ -310,6 +387,8 @@ std::string						SettingMenu::TranslateCategory					(const char* aCategoryName)
 		}
 	}
 
+	//Other generic setting categories
+	//NOTE: A space is added to ensure they bubble to the top of sorting
 	const char* settingscats[] = 
 	{
 		"qtrecord", " Video Recording",
@@ -328,6 +407,7 @@ std::string						SettingMenu::TranslateCategory					(const char* aCategoryName)
 		}
 	}
 
+	//No translation, use the default
 	return aCategoryName;
 }
 
--- mednafen/src/settingmenu.h
+++ mednafen/src/settingmenu.h
@@ -1,44 +1,83 @@
 #pragma once
 
+///ListView subclass for handling a SettingLineView's input.
 class								SettingLineView : public AnchoredListView
 {
 	public:
+		///Create a new SettingLineView.
+		///@param aList Weak pointer to the view's parent SummerfaceList.
 									SettingLineView						(SummerfaceList_WeakPtr aList) : AnchoredListView(aList), RefreshHeader(true) {}
+
+		///Blank virtual destructor for SettingLineView.
 		virtual						~SettingLineView					() {}
 
+		///Implement an Input method for ListView.
+		///@param aButton System button that was pressed.
+		///@return True to stop processing the interface.
 		bool						Input								(uint32_t aButton);
 
-		void						DoHeaderRefresh						();
+		///Refresh the parent list's header if needed.
+		///@param aList The parent list.
+		void						DoHeaderRefresh						(SummerfaceList_Ptr aList);
+
+		///Implement a DrawItem method for AnchoredListView.
+		///@param aList Parent list.
+		///@param aItem Item to draw.
+		///@param aX X position to draw the item.
+		///@param aY Y position to draw the item.
+		///@param aSelected True if this item is the currently selected item.
 		virtual bool				DrawItem							(SummerfaceList_Ptr aList, SummerfaceItem_Ptr aItem, uint32_t aX, uint32_t aY, bool aSelected);
 
+		///Handle changing a boolean setting.
+		///@param aButton System button that was pressed.
+		///@param aSetting Setting to update.
+		///@return True if this function has eaten the input, false to continue processing.
 		bool						HandleBool							(uint32_t aButton, const MDFNCS& aSetting);
+
+		///Handle changing an int or unsigned int setting.
+		///@param aButton System button that was pressed.
+		///@param aSetting Setting to update.
+		///@return True if this function has eaten the input, false to continue processing.
 		bool						HandleInt							(uint32_t aButton, const MDFNCS& aSetting);
+
+		///Handle changing an enumeration setting.
+		///@param aButton System button that was pressed.
+		///@param aSetting Setting to update.
+		///@return True if this function has eaten the input, false to continue processing.
 		bool						HandleEnum							(uint32_t aButton, const MDFNCS& aSetting);
 
 	private:
-		bool RefreshHeader;
+		bool						RefreshHeader;						///<When true DoHeaderRefresh will update the parent interfaces header with the currently selected item.
 };
 
-typedef std::map<std::string, std::vector<const MDFNCS*> >	SettingCollection;
-
-
+///Class used to list and edit mednafen's settings.
 class								SettingMenu
 {
+	typedef std::map<std::string, std::vector<const MDFNCS*> >	SettingCollection;	///<Collection type for the Settings cache
+
 	public:
+		///Create a new SettingMenu.
+		///@param aDefaultCategory The name of the default category. This category will be selected at load, and shaded in a special color.
 									SettingMenu							(const std::string& aDefaultCategory = std::string());
+
+		///Blank virtual destructor for SetttingMenu.
 		virtual						~SettingMenu						() {}
 
+		///Run the setting editor.
 		void						Do									();
 
 	private:
+		///Load and cache all of the settings from the mednafen core.
 		void						LoadSettings						();
+
+		///Translate a sparse category name to a friendly one.
 		std::string					TranslateCategory					(const char* aCategory);
 
 	private:
-		SummerfaceList_Ptr			List;
-		SummerfaceList_Ptr			CategoryList;
-		Summerface_Ptr				Interface;
+		SummerfaceList_Ptr			List;								///<SummerfaceList used for displaying a category of settings.
+		SummerfaceList_Ptr			CategoryList;						///<SummerfaceList used for displaying all categories.
+		Summerface_Ptr				Interface;							///<Summerface object used for the CategoryList.
 
-		SettingCollection			Settings;
+		SettingCollection			Settings;							///<The cache of settings from mednafen.
 };
 
