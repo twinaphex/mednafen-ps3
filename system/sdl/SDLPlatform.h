61bd678f0af1f0de3e390482a62ab8e231aa9542
Mike <Mike@localhost>
2011-07-17 23:26:39 -0400

    Some fixups to build for windows

--- system/sdl/SDLPlatform.h
+++ system/sdl/SDLPlatform.h
@@ -5,8 +5,15 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+#ifdef __WIN32__
+#include <GL/glew.h>
+#include <GL/gl.h>
+#endif
+
 #include <SDL/SDL.h>
+#ifndef __WIN32__
 #include <SDL/SDL_opengl.h>
+#endif
 #include <SDL/SDL_net.h>
 #include <Cg/cg.h>
 #include <Cg/cgGL.h>
