dbcbb9a038d4f8488be0f78eef6a65ac89d5446c
Mike <Mike@localhost>
2011-07-19 18:15:47 -0400

    Strip complexity out of the makefiles (PS3 probably broken right now); Add a 'pch' target that can be used to build a precompiled header out of 'pch.h' Add poorly timed key repeat to the GUI

--- system/src/ESInput.h
+++ system/src/ESInput.h
@@ -1,85 +1,94 @@
 #pragma once
 
-#define ES_BUTTON_UP 0xFFFFFF00ul
-#define ES_BUTTON_DOWN 0xFFFFFF01ul
-#define ES_BUTTON_LEFT 0xFFFFFF02ul
-#define ES_BUTTON_RIGHT 0xFFFFFF03ul
-#define ES_BUTTON_ACCEPT 0xFFFFFF04ul
-#define ES_BUTTON_CANCEL 0xFFFFFF05ul
-#define ES_BUTTON_SHIFT 0xFFFFFF06ul
-#define ES_BUTTON_TAB 0xFFFFFF07ul
-#define ES_BUTTON_AUXLEFT1 0xFFFFFF08ul
-#define ES_BUTTON_AUXRIGHT1 0xFFFFFF09ul
-#define ES_BUTTON_AUXLEFT2 0xFFFFFF0Aul
-#define ES_BUTTON_AUXRIGHT2 0xFFFFFF0Bul
-#define ES_BUTTON_AUXLEFT3 0xFFFFFF0Cul
-#define ES_BUTTON_AUXRIGHT3 0xFFFFFF0Dul
+#define ES_BUTTON_UP		0xFFFFFF00ul
+#define ES_BUTTON_DOWN		0xFFFFFF01ul
+#define ES_BUTTON_LEFT		0xFFFFFF02ul
+#define ES_BUTTON_RIGHT		0xFFFFFF03ul
+#define ES_BUTTON_ACCEPT	0xFFFFFF04ul
+#define ES_BUTTON_CANCEL	0xFFFFFF05ul
+#define ES_BUTTON_SHIFT		0xFFFFFF06ul
+#define ES_BUTTON_TAB		0xFFFFFF07ul
+#define ES_BUTTON_AUXLEFT1	0xFFFFFF08ul
+#define ES_BUTTON_AUXRIGHT1	0xFFFFFF09ul
+#define ES_BUTTON_AUXLEFT2	0xFFFFFF0Aul
+#define ES_BUTTON_AUXRIGHT2	0xFFFFFF0Bul
+#define ES_BUTTON_AUXLEFT3	0xFFFFFF0Cul
+#define ES_BUTTON_AUXRIGHT3	0xFFFFFF0Dul
 
 class				ESInput
 {
 	public:	//Helpers
-		struct								Button
+		///Class holding state and methods for a single input button.
+		class								Button
 		{
-			bool							Pressed;
-			bool							Inspected;
-			bool							Jammed;
-			uint32_t						PressedTime;
+			friend class					ESInput;
 
-			typedef	bool					(*RefreshFunction)		(uint32_t, uint32_t, uint32_t);
-			uint32_t						User1, User2, User3;
+			public:
+				///Callback type for refreshing the Button's pressed state.
+				typedef	bool				(*RefreshFunction)		(uint32_t, uint32_t, uint32_t);
 			
-			bool							(*Refresh)				(uint32_t aUser1, uint32_t aUser2, uint32_t aUser3);
-
+				///Create a new Button object.
+				///@param aCallback Function that will be called to determine the Button's state.
+				///@param aUser1 User data that will be passed to the callback.
+				///@param aUser2 User data that will be passed to the callback.
+				///@param aUser3 User data that will be passed to the callback.
 											Button					(RefreshFunction aCallback, uint32_t aUser1, uint32_t aUser2, uint32_t aUser3) :
-				Pressed(false),	
-				Inspected(true),
-				Jammed(true),
-				PressedTime(0),
-				User1(aUser1),
-				User2(aUser2),
-				User3(aUser3),
-				Refresh(aCallback)
-			{
-
-			}
-
-			///Set the pressed state of the button.
-			///@param aPressed True if the Button is pressed.
-			void							SetState				(bool aPressed)
-			{
-				//Update jammed state
-				Jammed = (Jammed && aPressed);
-
-				//Only if the state is changed
-				if(Pressed != aPressed)
+					Pressed(false),	
+					Inspected(true),
+					Jammed(true),
+					PressedTime(0),
+					User1(aUser1),
+					User2(aUser2),
+					User3(aUser3),
+					Refresh(aCallback)
 				{
-					Pressed = aPressed;
-					Inspected = false;
-//					PressedTime = Utility::GetTicks();
 				}
-			}
 
-			///Force the button to report not being pressed until the next time it is released.
-			void							Reset					()
-			{
-				Jammed = true;
-			}
+				///Set the pressed state of the button.
+				///@param aPressed True if the Button is pressed.
+				void						SetState				(bool aPressed); //External
 
-			///Get the pressed state of the button.
-			///@return True if the Button is pressed.
-			bool							GetState				()
-			{
-				return Jammed ? false : Pressed;
-			}
+				///Force the button to report not being pressed until the next time it is released.
+				void						Reset					()
+				{
+					Jammed = true;
+				}
 
-			///Get the pressed state of the button.
-			///@return True if the Button is pressed.
-			bool							GetStateInspected		()
-			{
-				bool result = Inspected ? false : Pressed;
-				Inspected = true;
-				return Jammed ? false : result;
-			}
+				///Get the pressed state of the button.
+				///@return True if the Button is pressed.
+				bool						GetState				()
+				{
+					return Jammed ? false : Pressed;
+				}
+
+				///Get the pressed state of the button.
+				///@return True if the Button is pressed.
+				bool						GetStateInspected		()
+				{
+					bool result = Inspected ? false : Pressed;
+					Inspected = true;
+					return Jammed ? false : result;
+				}
+
+				///Get the state of the button with a repeating pulse.
+				///@return True if the Button is pressed.
+				bool						GetStateRepeat			();
+
+			private:
+				bool						Pressed;				///<Set to true when the Button is pressed, false when the button is released.
+				bool						Inspected;				///<Set to true after a call to GetStateInspected, false when the button is released.
+				bool						Jammed;					///<Set to true after a call to Reset, false when the button is released.
+				uint32_t					PressedTime;			///<Internal timer used for GetStateRepeat.
+				uint32_t					User1;					///<User data for the callback.
+				uint32_t					User2;					///<User data for the callback.
+				uint32_t					User3;					///<User data for the callback.
+
+				///Callback function to refresh the Button's state.
+				///@param aUser1 A 32-bit user data.
+				///@param aUser2 A 32-bit user data.
+				///@param aUser3 A 32-bit user data.
+				///@return True if the Button is pressed, false if it is released.
+				bool						(*Refresh)				(uint32_t aUser1, uint32_t aUser2, uint32_t aUser3);
 		};
 		typedef std::vector<Button>			InputDevice;
 		typedef std::vector<InputDevice>	InputDeviceList;
@@ -90,37 +99,57 @@ class				ESInput
 		///Shutdown the libes Input system.
 		static void							Shutdown				();
 
+		///Wait for a system key to be pressed.
+		///@param aPad Input port to wait for.
+		///@param aGuarantee If true the routine will loop until a key is pressed.
+		///@return An ES_BUTTON constant if a key was found, 0xFFFFFFFF otherwise.
 		static uint32_t						WaitForESKey			(uint32_t aPad, bool aGuarantee);
 					
+		///Get the number of available input devices.
+		///@return The number of available input devices.
 		static uint32_t						PadCount				() {return Inputs.size() ? Inputs.size() : 1;};
-		static void							Reset					();
-		static void							Refresh					();
-		
-		static int32_t						GetAxis					(uint32_t aPad, uint32_t aAxis) {return 0;}
 
+		///Reset the state of all buttons. All buttons will report being as being not active until they are released
+		///and pressed again.
+		static void							Reset					();
+
+		///Poll the input system to update button status.
+		static void							Refresh					();
+
+		///Determine if a button is being held down.
+		///@param aPad Input device to check.
+		///@param aButton Button to check on device.
+		///@return True if the button is held down.
 		static inline bool					ButtonPressed			(uint32_t aPad, uint32_t aButton)
 		{
-			//Translate ES keys
-			if(aButton >= ES_BUTTON_UP && aButton <= ES_BUTTON_AUXRIGHT3)
-			{
-				aButton = ESKeyIndex[aButton & 0xFF];
-			}
-
-			//Fetch from main device
-			if(aButton < 0x10000 && aPad < Inputs.size())
-			{
-				return (aButton < Inputs[aPad].size()) ? Inputs[aPad][aButton].GetState() : false;
-			}
-			//Fetch from sub device
-			else if(((aButton - 0x10000) / 0x10000) < SubInputs.size())
-			{
-				uint32_t device = ((aButton - 0x10000) / 0x10000);
-				uint32_t button = aButton & 0xFFFF;
-				return (button < SubInputs[device].size()) ? SubInputs[device][button].GetState() : false;
-			}
+			Button* button = GetButton(aPad, aButton);
+			return button && button->GetState();
 		}
 
+		///Determine if a button was pressed. This function returns true only once for every time the button is pressed.
+		///@param aPad Input device to check.
+		///@param aButton Button to check on device.
+		///@return True if the button was pressed.
 		static inline bool					ButtonDown				(uint32_t aPad, uint32_t aButton)
+		{
+			Button* button = GetButton(aPad, aButton);
+			return button && button->GetStateInspected();
+		}	
+
+		///Scan a device and determine if any button, system or otherwise, is pressed.
+		///@param aPad Input device to scan.
+		///@return A button ID or 0xFFFFFFFF if no buttons were pressed.
+		static uint32_t						GetAnyButton			(uint32_t aPad);
+
+		///Unused
+		static void							RumbleOn				(uint32_t aBig, uint32_t aSmall) {};
+
+	private:
+		///Get a pointer to a Button on an input device.
+		///@param aPad Input device to scan.
+		///@param aButton Button index to retrieve.
+		///@return A pointer to a Button structure, or NULL if parameters are invalid.
+		static Button*						GetButton				(uint32_t aPad, uint32_t aButton)
 		{
 			//Translate ES keys
 			if(aButton >= ES_BUTTON_UP && aButton <= ES_BUTTON_AUXRIGHT3)
@@ -131,23 +160,22 @@ class				ESInput
 			//Fetch from main device
 			if(aButton < 0x10000 && aPad < Inputs.size())
 			{
-				return (aButton < Inputs[aPad].size()) ? Inputs[aPad][aButton].GetStateInspected() : false;
+				return &Inputs[aPad][aButton];
 			}
 			//Fetch from sub device
 			else if(((aButton - 0x10000) / 0x10000) < SubInputs.size())
 			{
 				uint32_t device = ((aButton - 0x10000) / 0x10000);
 				uint32_t button = aButton & 0xFFFF;
-				return (button < SubInputs[device].size()) ? SubInputs[device][button].GetStateInspected() : false;
+				return &SubInputs[device][button];
 			}
-		}	
-	
-		static uint32_t						GetAnyButton			(uint32_t aPad);
-		static void							RumbleOn				(uint32_t aBig, uint32_t aSmall);
 
-	private:
-		static InputDeviceList				Inputs;
-		static InputDeviceList				SubInputs;
-		static uint32_t						ESKeyIndex[14];
+			return 0;
+		}
+
+		static InputDeviceList				Inputs;					///<List of input devices.
+		static InputDeviceList				SubInputs;				///<List of sub input devices.
+		static uint32_t						ESKeyIndex[14];			///<Mapping of System keys to platform specific keys.
 };
+
 
