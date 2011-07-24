423c0fb92457896794f5e47da16a3859c78d44fb
Mike <Mike@localhost>
2011-07-24 10:14:28 -0400

    Make the text file viewer option more useful;

---
+++ mednafen/src/textviewer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+DEFINE_PTR_TYPE(FileSelect);
+DEFINE_PTR_TYPE(TextViewer);
+
+class						TextFileViewer
+{
+	public:
+							TextFileViewer				();
+		virtual				~TextFileViewer				() {}
+
+		int					HandleInput					(Summerface_Ptr aInterface, const std::string& aWindow, uint32_t aButton);
+
+		void				Display						();
+
+	private:
+		TextViewer_Ptr		Viewer;						///<Widget to display the text.
+		Summerface_Ptr		Interface;					///<Interface used for displaying the text file.
+		BookmarkList		Bookmarks;					///<Unused bookmark container.
+		FileSelect_Ptr		Browser;					///<Browser for selecting files.
+		bool				Loaded;						///<Is a text file loaded?
+};
+
