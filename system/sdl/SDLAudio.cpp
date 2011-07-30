a54f05dd381c421c4d710433c5dd6b7eebdef0d7
Mike <Mike@localhost>
2011-07-30 12:22:44 -0400

    Add experimental SoundTouch pitch correction to SDL's ESAudio (importing the soundtouch source tree in the process)

--- system/sdl/SDLAudio.cpp
+++ system/sdl/SDLAudio.cpp
@@ -1,4 +1,9 @@
 #include <es_system.h>
+#include <SoundTouch.h>
+
+//HACK: Put them IN esaudio later
+static soundtouch::SoundTouch st;
+static uint32_t auxStBuffer[48000];
 
 void					ESAudio::Initialize				()
 {
@@ -13,8 +18,11 @@ void					ESAudio::Initialize				()
 	SDL_PauseAudio(0);
 
 	Semaphore = es_threads->MakeSemaphore(1);
-}
 
+	//Soundtouch setup
+	st.setSampleRate(48000);
+	st.setChannels(2);
+}
 
 void					ESAudio::Shutdown				()
 {
@@ -30,6 +38,38 @@ void					ESAudio::ProcessAudioCallback	(void *userdata, Uint8 *stream, int len)
 	{
 		Semaphore->Post();
 	}
+}
+
+void					ESAudio::AddSamples				(const uint32_t* aSamples, uint32_t aCount)
+{
+	//Add samples to sound touch
+	st.putSamples((int16_t*)aSamples, aCount);
+
+	//Process any available samples 
+	int32_t sampleCount = st.numSamples();
+
+	if(sampleCount)
+	{
+		//Don't process too large of a block
+		sampleCount = st.receiveSamples((int16_t*)auxStBuffer, std::min(2048, sampleCount));
+
+		//Put them in the ringbuffer as normal
+		while(Buffer.GetBufferFree() < sampleCount)
+			Semaphore->Wait();
+
+		SDL_LockAudio();
+		Buffer.WriteData((uint32_t*)auxStBuffer, sampleCount);
+		SDL_UnlockAudio();
+	}
+}
+
+volatile int32_t		ESAudio::GetBufferAmount		() {return Buffer.GetBufferAmount();}
+volatile int32_t		ESAudio::GetBufferFree			() {return Buffer.GetBufferFree();}
+
+void					ESAudio::SetSpeed				(uint32_t aSpeed)
+{
+	//Tell sound touch about the new tempo
+	st.setTempo(aSpeed);
 }
 
 SDL_AudioSpec			ESAudio::Format;
