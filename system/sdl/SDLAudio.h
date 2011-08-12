4679e53b08df68a4435507d608abc9ea317aa762
Mike <Mike@localhost>
2011-08-12 14:39:08 -0400

    Don't pass sound through Soundtouch if the speed is not changed, hopefully fixing audio delay when not fast forwarding

--- system/sdl/SDLAudio.h
+++ system/sdl/SDLAudio.h
@@ -2,24 +2,32 @@
 
 #include "src/utility/AudioBuffer.h"
 
-class								ESAudio
+namespace	soundtouch
+{
+	class								SoundTouch;
+}
+
+class									ESAudio
 {
 	public:	
-		static void					Initialize				();
-		static void					Shutdown				();
+		static void						Initialize				();
+		static void						Shutdown				();
 
-		static void					AddSamples				(const uint32_t* aSamples, uint32_t aCount);
-		static volatile int32_t		GetBufferAmount			();
-		static volatile int32_t		GetBufferFree			();
-		static void					SetSpeed				(uint32_t aSpeed);
+		static void						AddSamples				(const uint32_t* aSamples, uint32_t aCount);
+		static volatile int32_t			GetBufferAmount			() {return Buffer.GetBufferAmount();}
+		static volatile int32_t			GetBufferFree			() {return Buffer.GetBufferFree();}
+		static void						SetSpeed				(uint32_t aSpeed);
 
 	protected:
-		static void					ProcessAudioCallback	(void *userdata, Uint8 *stream, int len);
+		static void						ProcessAudioCallback	(void *userdata, Uint8 *stream, int len);
 
-		static const int			BlockCount = 16;
-		static SDL_AudioSpec		Format;
+		static SDL_AudioSpec			Format;
 
-		static AudioBuffer<>		Buffer;
-		static ESSemaphore*			Semaphore;
+		static AudioBuffer<>			Buffer;
+		static ESSemaphore*				Semaphore;
+
+		static soundtouch::SoundTouch	PitchShifter;
+		static uint32_t					AuxBuffer[48000];
+		static uint32_t					Speed;
 };
 
