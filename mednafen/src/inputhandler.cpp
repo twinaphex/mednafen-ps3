613e79798939e19ac75e93af4468c2d1c62c9faf
Mike <Mike@localhost>
2011-09-05 09:55:54 -0400

    Support separate device and button mapping for each port

--- mednafen/src/inputhandler.cpp
+++ mednafen/src/inputhandler.cpp
@@ -14,7 +14,7 @@ namespace
 
 	const MDFNGI*								GameInfo;
 	uint32_t									RapidCount;
-	std::vector<InputInfo>						Inputs;
+	std::vector<InputInfo>						Inputs[16];
 	uint8_t										ControllerBits[16][256];
 }
 
@@ -22,9 +22,9 @@ namespace
 class									InputEnumeratorBase : public InputEnumerator
 {
 	public:
-										InputEnumeratorBase				() : GameInfo(0), PortInfo(0), DeviceInfo(0) {}
-		virtual void					System							(const MDFNGI* aDescription) {GameInfo = aDescription;}
-		virtual bool					Port							(const InputPortInfoStruct* aDescription) {PortInfo = aDescription; return true;}
+										InputEnumeratorBase				() : GameInfo(0), PortInfo(0), DeviceInfo(0), PortIndex(0) {}
+		virtual void					System							(const MDFNGI* aDescription) {GameInfo = aDescription; PortIndex = 0;}
+		virtual bool					Port							(const InputPortInfoStruct* aDescription) {PortInfo = aDescription; PortIndex ++; return true;}
 		virtual bool					Device							(const InputDeviceInfoStruct* aDescription) {DeviceInfo = aDescription; return true;}
 
 	protected:
@@ -32,6 +32,7 @@ class									InputEnumeratorBase : public InputEnumerator
 		const InputPortInfoStruct*		PortInfo;
 		const InputDeviceInfoStruct*	DeviceInfo;
 
+		uint32_t						PortIndex;
 		char							StringBuffer[1024];
 };
 
@@ -65,7 +66,7 @@ class									SettingGenerator : public InputEnumeratorBase
 			const char* defaultDeviceName = PortInfo->DefaultDevice ? PortInfo->DefaultDevice : PortInfo->DeviceInfo[0].ShortName;
 
 			//Stash the setting
-			MDFNSetting thisinput = {Utility::VAPrintD("%s.esinput.port1", GameInfo->shortname), MDFNSF_NOFLAGS, "Input.", NULL, MDFNST_ENUM, defaultDeviceName, 0, 0, 0, 0, devices};
+			MDFNSetting thisinput = {Utility::VAPrintD("%s.esinput.port%d", GameInfo->shortname, PortIndex), MDFNSF_NOFLAGS, "Input.", NULL, MDFNST_ENUM, defaultDeviceName, 0, 0, 0, 0, devices};
 			Settings.push_back(thisinput);
 
 			return true;
@@ -78,13 +79,13 @@ class									SettingGenerator : public InputEnumeratorBase
 			{
 				MDFNSetting	thisInput = {0, MDFNSF_CAT_INPUT, aDescription->Name, DeviceInfo->FullName, MDFNST_UINT, "0"};
 
-				thisInput.name = Utility::VAPrintD("%s.esinput.%s.%s", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName);
+				thisInput.name = Utility::VAPrintD("%s.esinput.port%d.%s.%s", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName);
 				Settings.push_back(thisInput);
 
 				//Rapid button support
 				if(aDescription->Type == IDIT_BUTTON_CAN_RAPID)
 				{
-					thisInput.name = Utility::VAPrintD("%s.esinput.%s.%s_rapid", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName);
+					thisInput.name = Utility::VAPrintD("%s.esinput.port%d.%s.%s_rapid", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName);
 					Settings.push_back(thisInput);
 				}
 			}
@@ -92,7 +93,6 @@ class									SettingGenerator : public InputEnumeratorBase
 
 	private:
 		std::vector<MDFNSetting>&		Settings;
-
 };
 
 //Configure the input buttons for a given system
@@ -118,7 +118,7 @@ class									ConfigurePrepper : public InputEnumeratorBase
 			InputEnumeratorBase::Port(aDescription);
 
 			const char* defaultDeviceName = PortInfo->DefaultDevice ? PortInfo->DefaultDevice : PortInfo->DeviceInfo[0].ShortName;
-			AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port1", GameInfo->shortname));
+			AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d", GameInfo->shortname, PortIndex));
 
 			return true;
 		}
@@ -127,12 +127,12 @@ class									ConfigurePrepper : public InputEnumeratorBase
 		{
 			if(aDescription->SettingName)
 			{
-				AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.%s.%s", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName));
+				AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d.%s.%s", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName));
 
 				//Rapid button support
 				if(aDescription->Type == IDIT_BUTTON_CAN_RAPID)
 				{
-					AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.%s.%s_rapid", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName));
+					AddSetting(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d.%s.%s_rapid", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName));
 				}
 			}
 		}
@@ -150,16 +150,24 @@ class									ConfigurePrepper : public InputEnumeratorBase
 class									SettingReader : public InputEnumeratorBase
 {
 	public:
-										SettingReader					() : bit(0) {}
+										SettingReader					() : BitIndex(0) {}
+
+
+		virtual bool					Port							(const InputPortInfoStruct* aDescription)
+		{
+			InputEnumeratorBase::Port(aDescription);
+			BitIndex = 0;
+			return true;
+		}
 
 		virtual bool					Device							(const InputDeviceInfoStruct* aDescription)
 		{
 			InputEnumeratorBase::Device(aDescription);
 
-			std::string type = MDFN_GetSettingS(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port1", GameInfo->shortname));
+			std::string type = MDFN_GetSettingS(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d", GameInfo->shortname, PortIndex));
 			if(type == DeviceInfo->ShortName)
 			{
-				MDFNI_SetInput(0, type.c_str(), &ControllerBits[0], 2);
+				MDFNI_SetInput(PortIndex - 1, type.c_str(), &ControllerBits[PortIndex - 1], 2);
 				return true;
 			}
 
@@ -173,25 +181,21 @@ class									SettingReader : public InputEnumeratorBase
 				InputInfo ii;
 				memset(&ii, 0, sizeof(ii));
 
-				ii.BitOffset = bit ++;
-				ii.Button = MDFN_GetSettingUI(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.%s.%s", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName));
+				ii.BitOffset = BitIndex ++;
+				ii.Button = MDFN_GetSettingUI(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d.%s.%s", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName));
 				if(aDescription->Type == IDIT_BUTTON_CAN_RAPID)
 				{
-					ii.RapidButton = MDFN_GetSettingUI(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.%s.%s_rapid", GameInfo->shortname, DeviceInfo->ShortName, aDescription->SettingName));
+					ii.RapidButton = MDFN_GetSettingUI(Utility::VAPrint(StringBuffer, sizeof(StringBuffer), "%s.esinput.port%d.%s.%s_rapid", GameInfo->shortname, PortIndex, DeviceInfo->ShortName, aDescription->SettingName));
 				}
 
 				ii.Data = aDescription;
-				Inputs.push_back(ii);
+				Inputs[PortIndex -1].push_back(ii);
 			}
 		}
 
 
 	private:
-		InputHandler*					Handler;
-
-		std::vector<const MDFNCS*>		Settings;
-
-		int								bit;
+		int								BitIndex;
 };
 
 void							InputHandler::SetGameInfo						(const MDFNGI* aSystem)
@@ -201,9 +205,9 @@ void							InputHandler::SetGameInfo						(const MDFNGI* aSystem)
 	ReadSettings();
 
 	//Run configure if all input values are zero
-	for(int i = 0; i != Inputs.size(); i ++)
+	for(int i = 0; i != Inputs[0].size(); i ++)
 	{
-		if(Inputs[i].Button != 0)
+		if(Inputs[0][i].Button != 0)
 		{
 			return;
 		}
@@ -218,20 +222,26 @@ void							InputHandler::EnumerateInputs					(const MDFNGI* aSystem, InputEnumer
 
 	aEnumerator->System(aSystem);
 
-	//Ports: TODO: Multiple ports
-	if(aEnumerator->Port(&aSystem->InputInfo->Types[0]))
+	//Ports
+	const InputPortInfoStruct* thisPort = aSystem->InputInfo->Types;
+	for(int port = 0; port != aSystem->InputInfo->InputPorts; port ++, thisPort ++)
 	{
-		//Devices
-		const InputDeviceInfoStruct* thisDevice = aSystem->InputInfo->Types[0].DeviceInfo;
-		for(int i = 0; i != aSystem->InputInfo->Types[0].NumTypes; i ++, thisDevice ++)
+		if(aEnumerator->Port(thisPort))
 		{
-			if(aEnumerator->Device(thisDevice))
+			//Devices
+			const InputDeviceInfoStruct* thisDevice = thisPort->DeviceInfo;
+			for(int i = 0; i != thisPort->NumTypes; i ++, thisDevice ++)
 			{
-				//Buttons
-				const InputDeviceInputInfoStruct* thisButton = thisDevice->IDII;
-				for(int j = 0; j != thisDevice->NumInputs; j ++, thisButton ++)
+				//TODO: Support port expanders?
+				if(thisDevice->IDII && aEnumerator->Device(thisDevice))
 				{
-					aEnumerator->Button(thisButton);
+					//Buttons
+					const InputDeviceInputInfoStruct* thisButton = thisDevice->IDII;
+					for(int j = 0; j != thisDevice->NumInputs; j ++, thisButton ++)
+					{
+
+						aEnumerator->Button(thisButton);
+					}
 				}
 			}
 		}
@@ -245,27 +255,27 @@ void							InputHandler::Process							()
 	//Update platform dependent input
 	ESInput::Refresh();
 
-	//TODO: Support more input types, more than two controllers
-	for(int p = 0; p != GameInfo->InputInfo->InputPorts && p != 2; p ++)
+	//TODO: Support more input types
+	for(int p = 0; p != GameInfo->InputInfo->InputPorts; p ++)
 	{
 		//Clear this controller
 		memset(ControllerBits[p], 0, 128);
 
 		//Update all of the buttons
-		for(int i = 0; i != Inputs.size(); i ++)
+		for(int i = 0; i != Inputs[p].size(); i ++)
 		{
 			//Calculate offset in ControllerBits
-			int byte = Inputs[i].BitOffset / 8;
-			int bit = Inputs[i].BitOffset & 7;
+			int byte = Inputs[p][i].BitOffset / 8;
+			int bit = Inputs[p][i].BitOffset & 7;
 
 			//Get any non rapid press
-			if((Inputs[i].Data->Type == IDIT_BUTTON || Inputs[i].Data->Type == IDIT_BUTTON_CAN_RAPID) && (ESInput::ButtonPressed(p, Inputs[i].Button)))
+			if((Inputs[p][i].Data->Type == IDIT_BUTTON || Inputs[p][i].Data->Type == IDIT_BUTTON_CAN_RAPID) && (ESInput::ButtonPressed(p, Inputs[p][i].Button)))
 			{
 				ControllerBits[p][byte] |= 1 << bit;
 			}
 
 			//Get any rapid press
-			if((Inputs[i].Data->Type == IDIT_BUTTON_CAN_RAPID) && (ESInput::ButtonPressed(p, Inputs[i].RapidButton) && (RapidCount == 0)))
+			if((Inputs[p][i].Data->Type == IDIT_BUTTON_CAN_RAPID) && (ESInput::ButtonPressed(p, Inputs[p][i].RapidButton) && (RapidCount == 0)))
 			{
 				ControllerBits[p][byte] |= 1 << bit;
 			}
@@ -286,7 +296,10 @@ void							InputHandler::Configure							()
 
 void							InputHandler::ReadSettings						()
 {
-	Inputs.clear();
+	for(int i = 0; i != 16; i ++)
+	{
+		Inputs[i].clear();
+	}
 
 	SettingReader reader;
 	EnumerateInputs(GameInfo, &reader);
